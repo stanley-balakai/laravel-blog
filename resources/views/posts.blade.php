8aada12f2d87e2b95237067dbba721a744029784
Stanley Balakai <sbalakai@gmail.com>
2023-04-12 14:22:04 -0400

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro vitae corporis magnam mollitia commodi tempora voluptatum, enim in unde. Quas, eum. Dolor expedita enim autem alias, hic earum temporibus eum.
+        </p>
+
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro vitae corporis magnam mollitia commodi tempora voluptatum, enim in unde. Quas, eum. Dolor expedita enim autem alias, hic earum temporibus eum.
+        </p>
+
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro vitae corporis magnam mollitia commodi tempora voluptatum, enim in unde. Quas, eum. Dolor expedita enim autem alias, hic earum temporibus eum.
+        </p>
+
+    </article>
+</body>

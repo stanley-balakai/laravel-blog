4405fa39a4e0b110287478440051961b412b4a88
Stanley Balakai <sbalakai@gmail.com>
2023-04-14 10:11:24 -0400

    finished lesson 11

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,30 +5,10 @@
     <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro vitae corporis magnam mollitia commodi tempora voluptatum, enim in unde. Quas, eum. Dolor expedita enim autem alias, hic earum temporibus eum.
-        </p>
-
-    </article>
+    <?php foreach ($posts as $post) : ?>
 
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro vitae corporis magnam mollitia commodi tempora voluptatum, enim in unde. Quas, eum. Dolor expedita enim autem alias, hic earum temporibus eum.
-        </p>
-
+        <?= $post; ?>
     </article>
-
-    <article>
-        <h1><a href="/post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro vitae corporis magnam mollitia commodi tempora voluptatum, enim in unde. Quas, eum. Dolor expedita enim autem alias, hic earum temporibus eum.
-        </p>
-
-    </article>
+    <?php endforeach; ?>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +16,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
-});
 
-Route::get('post', function () {
-    return view('post', [
-        'post' => 'My First Post'
+    return view('posts', [
+        'posts' => Post::all()
     ]);
 });
+
+
+
+Route::get('posts/{post}', function ($slug)  {
+
+    $post = Post::find($slug);
+
+    return view('post', [
+        'post' => $post
+    ]);
+
+}) ->where('post', '[A-z-]+');
